9ab3b1d46277c97d7b9488fc468279a1792db53f
CengizhanParlak <cengizhan.parlak@hotmail.com>
2023-07-17 00:05:19 +0300

    update: extension updates

--- lib/core/utils/extensions/datetime_extension.dart
+++ lib/core/utils/extensions/datetime_extension.dart
@@ -1,5 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:ui_test_cengizhanparlak/app/service/language_service.dart';
 
 extension DateTimeExtension on DateTime {
-  String get toFormattedString => DateFormat.yMMMMd().format(this);
+  String get toFormattedString =>
+      DateFormat.yMMMMd(LanguageService.appLocale.toLanguageTag()).format(this);
 }
