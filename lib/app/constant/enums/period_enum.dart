da0635cb2c202231f9661e1d94d3cf7e45d0119f
CengizhanParlak <cengizhan.parlak@hotmail.com>
2023-07-16 20:34:42 +0300

    add: doc comment

--- lib/app/constant/enums/period_enum.dart
+++ lib/app/constant/enums/period_enum.dart
@@ -1,3 +1,7 @@
+/// Used to indicate the period of time which the most popular articles are to be
+/// retrieved from API.
+///
+/// Options are: [day], [week] and [month]
 enum Period {
   day(1),
   week(7),
