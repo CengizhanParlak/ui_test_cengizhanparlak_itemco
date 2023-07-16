f53de78dbc2d7c388c8e24f2bec2fb4b18ebface
CengizhanParlak <cengizhan.parlak@hotmail.com>
2023-07-16 22:34:02 +0300

    update: extension getter names

--- lib/core/utils/extensions/context_extension.dart
+++ lib/core/utils/extensions/context_extension.dart
@@ -8,16 +8,16 @@ extension BuildContextExtension on BuildContext {
 
   ThemeData get theme => Theme.of(this);
   TextTheme get textTheme => Theme.of(this).textTheme;
-  TextStyle get headline1 => Theme.of(this).textTheme.displayLarge!;
-  TextStyle get headline2 => Theme.of(this).textTheme.displayMedium!;
-  TextStyle get headline3 => Theme.of(this).textTheme.displaySmall!;
-  TextStyle get headline4 => Theme.of(this).textTheme.headlineMedium!;
-  TextStyle get headline5 => Theme.of(this).textTheme.headlineSmall!;
-  TextStyle get headline6 => Theme.of(this).textTheme.titleLarge!;
-  TextStyle get subtitle1 => Theme.of(this).textTheme.titleMedium!;
-  TextStyle get subtitle2 => Theme.of(this).textTheme.titleSmall!;
-  TextStyle get bodyText1 => Theme.of(this).textTheme.bodyLarge!;
-  TextStyle get bodyText2 => Theme.of(this).textTheme.bodyMedium!;
+  TextStyle get displayLarge => Theme.of(this).textTheme.displayLarge!;
+  TextStyle get displayMedium => Theme.of(this).textTheme.displayMedium!;
+  TextStyle get displaySmall => Theme.of(this).textTheme.displaySmall!;
+  TextStyle get headlineMedium => Theme.of(this).textTheme.headlineMedium!;
+  TextStyle get headlineSmall => Theme.of(this).textTheme.headlineSmall!;
+  TextStyle get titleLarge => Theme.of(this).textTheme.titleLarge!;
+  TextStyle get titleMedium => Theme.of(this).textTheme.titleMedium!;
+  TextStyle get titleSmall => Theme.of(this).textTheme.titleSmall!;
+  TextStyle get bodyLarge => Theme.of(this).textTheme.bodyLarge!;
+  TextStyle get bodyMedium => Theme.of(this).textTheme.bodyMedium!;
 
   double widthByRatio({required double ratio}) => width * ratio;
 
