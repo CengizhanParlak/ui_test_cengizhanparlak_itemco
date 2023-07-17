e92d57f49aa49f1d1f400db8fd718d181da06a37
CengizhanParlak <cengizhan.parlak@hotmail.com>
2023-07-17 03:41:33 +0300

    add: translation

--- lib/app/screens/article/components/article_screen_fab.dart
+++ lib/app/screens/article/components/article_screen_fab.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ui_test_cengizhanparlak/app/widgets/translated_text.dart';
 import 'package:ui_test_cengizhanparlak/core/utils/extensions/context_extension.dart';
 import 'package:ui_test_cengizhanparlak/core/utils/extensions/textstyle_extension.dart';
+import 'package:ui_test_cengizhanparlak/generated/locale_keys.g.dart';
 
 class ArticleScreenFAB extends ConsumerWidget {
   const ArticleScreenFAB({
@@ -23,7 +25,7 @@ class ArticleScreenFAB extends ConsumerWidget {
           : const Icon(Icons.article, color: Colors.white),
       label: isFromHTML
           ? Text('HTML', style: context.bodyMedium.white)
-          : Text('Brief', style: context.bodyMedium.white),
+          : TranslatedText(LocaleKeys.brief, style: context.bodyMedium.white),
     );
   }
 }
--- lib/generated/locale_keys.g.dart
+++ lib/generated/locale_keys.g.dart
@@ -2,5 +2,6 @@
 
 abstract class  LocaleKeys {
   static const mostPopularText = 'mostPopularText';
+  static const brief = 'brief';
 
 }
